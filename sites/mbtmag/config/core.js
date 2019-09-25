fd6e9767e75e1e8e34b04eb458618887bb6d5777
Josh Worden <solocommand@gmail.com>
2019-09-25 11:11:12 -0500

    Add Manufacturing Business Technology

---
+++ sites/mbtmag/config/core.js
@@ -0,0 +1,3 @@
+module.exports = {
+  siteName: 'Manufacturing Business Technology',
+};
---
+++ sites/mbtmag/config/navigation.js
@@ -0,0 +1,69 @@
+module.exports = {
+  primary: {
+    items: [
+      { href: '/best-practices', label: 'Best Practices' },
+      { href: '/business-intelligence', label: 'Business Intelligence' },
+      { href: '/cloud-computing', label: 'Cloud Computing' },
+      { href: '/crm', label: 'CRM' },
+      { href: '/erp', label: 'ERP' },
+      { href: '/global', label: 'Global' },
+      { href: '/quality-control', label: 'Quality Control' },
+      { href: '/security', label: 'Security' },
+    ],
+  },
+  secondary: {
+    items: [
+      { href: '/subscribe', label: 'Subscribe' },
+      { href: '/magazine', label: 'Magazine' },
+      { href: '/videos', label: 'Videos' },
+      { href: '/white-papers', label: 'White Papers' },
+      { href: '/webcasts', label: 'Webcasts' },
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/page/about-us', label: 'About Us' },
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: '/page/advertise', label: 'Advertise' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+        { href: '/best-practices', label: 'Best Practices' },
+        { href: '/business-intelligence', label: 'Business Intelligence' },
+        { href: '/cloud-computing', label: 'Cloud Computing' },
+        { href: '/crm', label: 'CRM' },
+        { href: '/erp', label: 'ERP' },
+        { href: '/global', label: 'Global' },
+        { href: '/quality-control', label: 'Quality Control' },
+        { href: '/security', label: 'Security' },
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/videos', label: 'Videos' },
+        { href: '/white-papers', label: 'White Papers' },
+        { href: '/webcasts', label: 'Webcasts' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: '/page/about-us', label: 'About Us' },
+        { href: '/contact-us', label: 'Contact Us' },
+      ],
+    },
+  ],
+};
