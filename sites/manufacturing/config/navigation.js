54ec1ef649deb0ea0607a1230d6cfe15eb537046
Josh Worden <solocommand@gmail.com>
2019-09-25 11:21:25 -0500

    Add manufacturing.net

---
+++ sites/manufacturing/config/navigation.js
@@ -0,0 +1,87 @@
+module.exports = {
+  primary: {
+    items: [
+      { href: '/aerospace', label: 'Aerospace' },
+      { href: '/automation', label: 'Automation' },
+      { href: '/automotive', label: 'Automotive' },
+      { href: '/chemical-processing', label: 'Chemical Processing' },
+      { href: '/e-commerce', label: 'E-Commerce' },
+      { href: '/economics', label: 'Economics' },
+      { href: '/energy', label: 'Energy' },
+      { href: '/iot', label: 'IOT' },
+      { href: '/labor', label: 'Labor' },
+      { href: '/laws-regulations', label: 'Laws & Regulations' },
+      { href: '/operations', label: 'Operations' },
+      { href: '/recalls', label: 'Recalls' },
+      { href: '/safety', label: 'Safety' },
+      { href: '/software', label: 'Software' },
+      { href: '/supply-chain', label: 'Supply Chain' },
+      { href: '/technology', label: 'Technology' },
+      { href: '/trade', label: 'Trade' },
+    ],
+  },
+  secondary: {
+    items: [
+      { href: '/subscribe', label: 'Subscribe' },
+      { href: '/magazine', label: 'Magazine' },
+      { href: '/videos', label: 'Videos' },
+      { href: '/white-papers', label: 'White Papers' },
+      { href: '/webcasts', label: 'Webcasts' },
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/page/about-us', label: 'About Us' },
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: '/page/advertise', label: 'Advertise' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+        { href: '/aerospace', label: 'Aerospace' },
+        { href: '/automation', label: 'Automation' },
+        { href: '/automotive', label: 'Automotive' },
+        { href: '/chemical-processing', label: 'Chemical Processing' },
+        { href: '/e-commerce', label: 'E-Commerce' },
+        { href: '/economics', label: 'Economics' },
+        { href: '/energy', label: 'Energy' },
+        { href: '/iot', label: 'IOT' },
+        { href: '/labor', label: 'Labor' },
+        { href: '/laws-regulations', label: 'Laws & Regulations' },
+        { href: '/operations', label: 'Operations' },
+        { href: '/recalls', label: 'Recalls' },
+        { href: '/safety', label: 'Safety' },
+        { href: '/software', label: 'Software' },
+        { href: '/supply-chain', label: 'Supply Chain' },
+        { href: '/technology', label: 'Technology' },
+        { href: '/trade', label: 'Trade' },
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/videos', label: 'Videos' },
+        { href: '/white-papers', label: 'White Papers' },
+        { href: '/webcasts', label: 'Webcasts' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: '/page/about-us', label: 'About Us' },
+        { href: '/contact-us', label: 'Contact Us' },
+      ],
+    },
+  ],
+};
