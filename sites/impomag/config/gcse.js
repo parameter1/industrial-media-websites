eb401f7341c7ebb03bfb52ad7aedaa3db0e9ac9c
Josh Worden <solocommand@gmail.com>
2019-09-25 11:01:54 -0500

    Add IMPO

---
+++ sites/impomag/config/gcse.js
@@ -0,0 +1,5 @@
+const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
+
+const config = new GCSEConfiguration('<NOT_SET>');
+
+module.exports = config;
---
+++ sites/impomag/config/navigation.js
@@ -0,0 +1,67 @@
+module.exports = {
+  primary: {
+    items: [
+      { href: '/ehs', label: 'EHS' },
+      { href: '/inventory-management', label: 'Inventory Management' },
+      { href: '/maintenance', label: 'Maintenance' },
+      { href: '/material-handling', label: 'Material Handling' },
+      { href: '/products', label: 'Products' },
+      { href: '/training-development', label: 'Training & Development' },
+      { href: '/warehouse', label: 'Warehouse' },
+    ],
+  },
+  secondary: {
+    items: [
+      { href: '/subscribe', label: 'Subscribe' },
+      { href: '/magazine', label: 'Magazine' },
+      { href: '/videos', label: 'Videos' },
+      { href: '/white-papers', label: 'White Papers' },
+      { href: '/webcasts', label: 'Webcasts' },
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/page/about-us', label: 'About Us' },
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: '/page/advertise', label: 'Advertise' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+        { href: '/ehs', label: 'EHS' },
+        { href: '/inventory-management', label: 'Inventory Management' },
+        { href: '/maintenance', label: 'Maintenance' },
+        { href: '/material-handling', label: 'Material Handling' },
+        { href: '/products', label: 'Products' },
+        { href: '/training-development', label: 'Training & Development' },
+        { href: '/warehouse', label: 'Warehouse' },
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/magazine', label: 'Magazine' },
+        { href: '/videos', label: 'Videos' },
+        { href: '/white-papers', label: 'White Papers' },
+        { href: '/webcasts', label: 'Webcasts' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: '/page/about-us', label: 'About Us' },
+        { href: '/contact-us', label: 'Contact Us' },
+      ],
+    },
+  ],
+};
