bdce2c6ecbd43159cac885372617dc378f077d84
Brian Miller <bmiller@southcomm.com>
2019-11-20 15:14:04 -0600

    add gcse configs to indm sites

--- sites/foodmanufacturing/config/gcse.js
+++ sites/foodmanufacturing/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('<NOT_SET>');
+const config = new GCSEConfiguration('003355913687346718228:w3ie9ydeqtb');
 
 module.exports = config;
