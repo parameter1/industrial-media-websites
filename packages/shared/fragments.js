e756f6b53cea5dff3fa4c3f75774b61c38848052
Jacob Bare <jacob@limit0.io>
2020-03-16 12:27:54 -0500

    Create initial fragments

--- packages/shared/fragments.js
+++ packages/shared/fragments.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const sharedContentList = require('./graphql/fragments/content-list');
+const sharedContentLatest = require('./graphql/fragments/content-latest');
+const sharedMagazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
+
+module.exports = {
+  sharedContentList,
+  sharedContentLatest,
+  sharedMagazineIssueArchive,
+};
