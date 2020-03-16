37a6d53290f941713e4463dd93184e46c51a546e
Jacob Bare <jacob@limit0.io>
2020-03-16 13:38:29 -0500

    Initially use shared package start

--- sites/ien.com/server/routes/index.js
+++ sites/ien.com/server/routes/index.js
@@ -1,39 +1,34 @@
-const contactUsHandler = require('@industrial-media/package-common/contact-us');
 const home = require('./home');
-const content = require('./content');
-const dynamicPages = require('./dynamic-page');
-const magazine = require('./magazine');
-const publishedContent = require('./published-content');
-const search = require('./search');
-const subscribe = require('./subscribe');
-const websiteSections = require('./website-section');
+// const content = require('./content');
+// const dynamicPages = require('./dynamic-page');
+// const magazine = require('./magazine');
+// const publishedContent = require('./published-content');
+// const search = require('./search');
+// const subscribe = require('./subscribe');
+// const websiteSections = require('./website-section');
 
 module.exports = (app) => {
-  // Handle contact is submissions on /__contact-us
-  // @todo This should be removed once contact-us is moved to core.
-  contactUsHandler(app);
-
   // Homepage
   home(app);
 
-  // Magazine Pages
-  magazine(app);
+  // // Magazine Pages
+  // magazine(app);
 
-  // Dynamic Pages
-  dynamicPages(app);
+  // // Dynamic Pages
+  // dynamicPages(app);
 
-  // Content Pages
-  content(app);
+  // // Content Pages
+  // content(app);
 
-  // Published Content Pages
-  publishedContent(app);
+  // // Published Content Pages
+  // publishedContent(app);
 
-  // Search
-  search(app);
+  // // Search
+  // search(app);
 
-  // Subscription Pages
-  subscribe(app);
+  // // Subscription Pages
+  // subscribe(app);
 
-  // Website Sections
-  websiteSections(app);
+  // // Website Sections
+  // websiteSections(app);
 };
