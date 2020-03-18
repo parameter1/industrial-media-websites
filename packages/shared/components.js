3b68bdb621f4e2729ed9e47ee97eada6d4663fa8
Jacob Bare <jacob@limit0.io>
2020-03-18 13:59:11 -0500

    Use cards in content feed

--- packages/shared/components.js
+++ packages/shared/components.js
@@ -1,5 +1,7 @@
 const SharedContentListFeedFlow = require('./components/flows/content/list-feed');
+const SharedContentCardDeckFeedFlow = require('./components/flows/content/card-deck-feed');
 
 module.exports = {
   SharedContentListFeedFlow,
+  SharedContentCardDeckFeedFlow,
 };
