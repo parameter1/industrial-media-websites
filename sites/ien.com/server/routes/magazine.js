ccbf7c51658c7621c14dded9ea044df994d75cca
Jacob Bare <jacob@limit0.io>
2020-03-18 16:00:57 -0500

    Handle magazine pages

--- sites/ien.com/server/routes/magazine.js
+++ sites/ien.com/server/routes/magazine.js
@@ -1,9 +1,10 @@
 const { withMagazineIssue, withMagazinePublication } = require('@base-cms/marko-web/middleware');
-const index = require('@industrial-media/package-shared/templates/magazine');
 const publication = require('@industrial-media/package-shared/templates/magazine/publication');
 const publicationFragment = require('@industrial-media/package-shared/graphql/fragments/magazine-publication-page');
 const issue = require('@industrial-media/package-shared/templates/magazine/issue');
 const issueFragment = require('@industrial-media/package-shared/graphql/fragments/magazine-issue-page');
+
+const index = require('../templates/magazine/index');
 
 module.exports = (app) => {
   app.get('/magazine', (req, res) => {
