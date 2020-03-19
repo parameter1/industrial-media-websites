6851d3abf5465f6fcf95461cddbe93bce22ddbad
Jacob Bare <jacob@limit0.io>
2020-03-19 13:31:18 -0500

    Apply shared browser, styles, and start

--- sites/designdevelopmenttoday.com/index.js
+++ sites/designdevelopmenttoday.com/index.js
@@ -1,13 +1,8 @@
-const newrelic = require('newrelic');
-const { startServer } = require('@base-cms/marko-web');
-const trackReferrer = require('@industrial-media/package-common/exact-target/track-referrer');
-const { version } = require('./package.json');
+const startServer = require('@industrial-media/package-shared/start-server');
+
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
-const document = require('./server/components/document');
-const components = require('./server/components');
-const fragments = require('./server/fragments');
 
 const { log } = console;
 
@@ -16,13 +11,4 @@ module.exports = startServer({
   coreConfig,
   siteConfig,
   routes,
-  document,
-  components,
-  fragments,
-  version,
-  onStart: (app) => {
-    app.set('trust proxy', 'loopback, linklocal, uniquelocal');
-    app.use(trackReferrer({ onError: newrelic.noticeError }));
-  },
-  onAsyncBlockError: e => newrelic.noticeError(e),
 }).then(() => log('Website started!')).catch(e => setImmediate(() => { throw e; }));
