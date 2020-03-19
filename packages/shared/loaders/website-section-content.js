fbf152c0757e70a0114a592280c84d9ab030f66c
Jacob Bare <jacob@limit0.io>
2020-03-19 14:01:47 -0500

    Use optioned content for section query

--- packages/shared/loaders/website-section-content.js
+++ packages/shared/loaders/website-section-content.js
@@ -1,4 +1,4 @@
-const { websiteScheduledContent } = require('@base-cms/web-common/block-loaders');
+const { websiteOptionedContent } = require('@base-cms/web-common/block-loaders');
 const defaultFragment = require('../graphql/fragments/content-list');
 
 const loadFeaturedContent = async (apolloClient, params = {}) => {
@@ -8,7 +8,7 @@ const loadFeaturedContent = async (apolloClient, params = {}) => {
     queryFragment: defaultFragment,
   };
 
-  return websiteScheduledContent(apolloClient, {
+  return websiteOptionedContent(apolloClient, {
     ...defaults,
     ...params,
   });
@@ -21,7 +21,7 @@ const loadStandardContent = async (apolloClient, params = {}) => {
     queryFragment: defaultFragment,
   };
 
-  return websiteScheduledContent(apolloClient, {
+  return websiteOptionedContent(apolloClient, {
     ...defaults,
     ...params,
   });
