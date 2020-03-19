d7d0edb704556a32950c1f4679e9fb08fa6d6d64
Jacob Bare <jacob@limit0.io>
2020-03-19 08:49:48 -0500

    Remove unused routes/templates

--- sites/ien.com/server/routes/index.js
+++ sites/ien.com/server/routes/index.js
@@ -2,9 +2,7 @@ const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
 const magazine = require('./magazine');
-// const publishedContent = require('./published-content');
 // const search = require('./search');
-// const subscribe = require('./subscribe');
 const websiteSections = require('./website-section');
 
 module.exports = (app) => {
@@ -20,14 +18,8 @@ module.exports = (app) => {
   // Content Pages
   content(app);
 
-  // // Published Content Pages
-  // publishedContent(app);
-
   // // Search
   // search(app);
-
-  // // Subscription Pages
-  // subscribe(app);
 
   // Website Sections
   websiteSections(app);
