9253fe5c15bfa4e38554f036c8692aa16e077918
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-24 08:20:44 -0500

    Update IEN, CEN, DDT ad units

--- sites/ien.com/config/gam.js
+++ sites/ien.com/config/gam.js
@@ -1,21 +1,18 @@
 const configureGAM = require('@industrial-media/package-shared/config/gam');
 
-const config = configureGAM({ basePath: '' });
+const config = configureGAM({ basePath: 'IEN' });
 
 config
   .setAliasAdUnits('default', [
-    { name: 'lb1', templateName: 'LB', path: 'IEN-SL' },
-    { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'IEN-SL' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'IEN-MR' },
-    { name: 'rail2', templateName: 'RAIL2', path: 'IEN-MR' },
-
-    { name: 'billboard1', templateName: 'BILLBOARD', path: 'IEN970x250' },
-
-    { name: 'infinite-rail', templateName: 'INFINITE-RAIL', path: 'IEN-MR' },
-    { name: 'infinite-interstitial', templateName: 'INFINITE-INTERSTITIAL', path: 'IEN-MR' },
-    { name: 'in-content', templateName: 'IN-CONTENT', path: 'IEN-MR' },
-
-    { name: 'reskin', path: 'IEN-Reskin' },
+    { name: 'lb1', templateName: 'LB', path: 'default_lb1' },
+    { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'default_lb1' },
+    { name: 'billboard1', templateName: 'BILLBOARD', path: 'default_billboard' },
+    { name: 'rail1', templateName: 'RAIL1', path: 'default_rail1' },
+    { name: 'rail2', templateName: 'RAIL2', path: 'default_rail2' },
+    { name: 'infinite-rail', templateName: 'INFINITE-RAIL', path: 'default_rail1' },
+    { name: 'infinite-interstitial', templateName: 'INFINITE-INTERSTITIAL', path: 'default_rail1' },
+    { name: 'in-content', templateName: 'IN-CONTENT', path: 'default_rail1' },
+    { name: 'reskin', path: 'default_reskin' },
     { name: 'wa', path: 'default_wa' },
   ]);
 
