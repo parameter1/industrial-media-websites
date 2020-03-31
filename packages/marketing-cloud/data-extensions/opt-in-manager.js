3442177a96cc534b6a6866d8b5e9bab5fc7e8565
Jacob Bare <jacob@limit0.io>
2020-03-31 14:20:51 -0500

    Move into standalone functions

--- packages/marketing-cloud/data-extensions/opt-in-manager.js
+++ packages/marketing-cloud/data-extensions/opt-in-manager.js
@@ -1,26 +1,29 @@
 const soap = require('../soap');
 
+/**
+ * If the DE does not exist, this method will return null.
+ */
+const getDataExtension = ({
+  customerKey,
+  props = ['ObjectID', 'CustomerKey', 'Name'],
+} = {}) => soap.retrieveByCustomerKey('DataExtension', customerKey, props);
+
+/**
+ * If the DE does not exist, this method will throw an error.
+ * If the subscriber is not found on the DE, this method will return null.
+ */
+const getDataExtensionSubscriber = async ({ customerKey, subscriberKey, field = 'email' } = {}) => {
+  const filter = {
+    attributes: { 'xsi:type': 'SimpleFilterPart' },
+    Property: field,
+    SimpleOperator: 'equals',
+    Value: subscriberKey,
+  };
+
+  return soap.retrieveOne(`DataExtensionObject[${customerKey}]`, filter, [field]);
+};
+
 module.exports = {
-  /**
-   * If the DE does not exist, this method will return null.
-   */
-  getDataExtension: ({
-    customerKey,
-    props = ['ObjectID', 'CustomerKey', 'Name'],
-  } = {}) => soap.retrieveByCustomerKey('DataExtension', customerKey, props),
-
-  /**
-   * If the DE does not exist, this method will throw an error.
-   * If the subscriber is not found on the DE, this method will return null.
-   */
-  getDataExtensionSubscriber: async ({ customerKey, subscriberKey, field = 'email' } = {}) => {
-    const filter = {
-      attributes: { 'xsi:type': 'SimpleFilterPart' },
-      Property: field,
-      SimpleOperator: 'equals',
-      Value: subscriberKey,
-    };
-
-    return soap.retrieveOne(`DataExtensionObject[${customerKey}]`, filter, [field]);
-  },
+  getDataExtension,
+  getDataExtensionSubscriber,
 };
