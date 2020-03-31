97ae8b480ea6bf3fde1768417cf6cab52d705a37
Jacob Bare <jacob@limit0.io>
2020-03-31 16:01:41 -0500

    Add sendOptStatus method to upsert subscriber to de

--- packages/marketing-cloud/data-extensions/opt-in-manager.js
+++ packages/marketing-cloud/data-extensions/opt-in-manager.js
@@ -23,7 +23,57 @@ const getDataExtensionSubscriber = async ({ customerKey, subscriberKey, field = 
   return soap.retrieveOne(`DataExtensionObject[${customerKey}]`, filter, [field]);
 };
 
+const getDataExtensionFields = async ({ customerKey, props = ['Name'] } = {}) => {
+  const Filter = {
+    attributes: { 'xsi:type': 'SimpleFilterPart' },
+    Property: 'DataExtension.CustomerKey',
+    SimpleOperator: 'equals',
+    Value: customerKey,
+  };
+  const { Results } = await soap.retrieve('DataExtensionField', props, { Filter });
+  return Results;
+};
+
+const sendOptStatus = async ({
+  extensionKey,
+  email,
+  data = {},
+  optedIn,
+}) => {
+  const [extensionSubscriber, fields] = await Promise.all([
+    getDataExtensionSubscriber({
+      customerKey: extensionKey,
+      subscriberKey: email,
+    }),
+    getDataExtensionFields({ customerKey: extensionKey }),
+  ]);
+
+  // only include the data if the corresponding field exists in the data extension.
+  // set an empty string as the default value (to prevent non-nullable field errors).
+  const row = {
+    ...fields.reduce((o, field) => {
+      const { Name } = field;
+      const value = data[Name] || '';
+      return { ...o, [Name]: value };
+    }, {}),
+    email,
+    optedIn: Number(optedIn),
+  };
+
+  const payload = {
+    CustomerKey: extensionKey,
+    Properties: Object.keys(row).map((key) => {
+      const value = row[key];
+      return { Property: { Name: key, Value: value } };
+    }),
+  };
+
+  if (extensionSubscriber) return soap.update('DataExtensionObject', payload);
+  return soap.create('DataExtensionObject', payload);
+};
+
 module.exports = {
+  sendOptStatus,
   getDataExtension,
   getDataExtensionSubscriber,
 };
