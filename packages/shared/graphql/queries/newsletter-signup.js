2009213551089c84feb9853def8f3d029b4096b8
Jacob Bare <jacob@limit0.io>
2020-03-31 13:25:21 -0500

    Revert to default sort

--- packages/shared/graphql/queries/newsletter-signup.js
+++ packages/shared/graphql/queries/newsletter-signup.js
@@ -3,7 +3,7 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 query EmailNewsletterSignup($siteId: ObjectID!) {
-  emailNewsletters(input: { siteId: $siteId, pagination: { limit: 0 }, sort: { field: name, order: asc } }) {
+  emailNewsletters(input: { siteId: $siteId, pagination: { limit: 0 } }) {
     edges {
       node {
         id
