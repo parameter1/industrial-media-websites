9884050bd817e3a3cd0dbe20341b47c7e9a20f3b
Jacob Bare <jacob@limit0.io>
2020-04-01 12:05:24 -0500

    Update mnet routes and templates

--- sites/manufacturing.net/server/routes/website-section.js
+++ sites/manufacturing.net/server/routes/website-section.js
@@ -1,7 +1,7 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const section = require('../templates/website-section');
-const contactUs = require('../templates/website-section/contact-us');
-const queryFragment = require('../graphql/fragments/website-section-page');
+const section = require('@industrial-media/package-shared/templates/website-section');
+const contactUs = require('@industrial-media/package-shared/templates/website-section/contact-us');
+const queryFragment = require('@industrial-media/package-shared/graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
@@ -10,7 +10,6 @@ module.exports = (app) => {
   }));
 
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
-    // Replace with ID resolver or website domain something
     template: section,
     queryFragment,
   }));
