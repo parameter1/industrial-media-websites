ef688f99d2403405582f88514f7df1190b6ceb30
Jacob Bare <jacob@limit0.io>
2020-04-06 10:47:48 -0500

    Disable comments on DDT

--- sites/designdevelopmenttoday.com/config/site.js
+++ sites/designdevelopmenttoday.com/config/site.js
@@ -27,7 +27,7 @@ module.exports = {
   identityX: {
     appId: '5e28a3dd58e67b229e55ae43',
     enabled: true,
-    comments: { enabled: true },
+    comments: { enabled: false },
     options: {
       appContextId: '5e7e610375a0950125baf2b3',
     },
