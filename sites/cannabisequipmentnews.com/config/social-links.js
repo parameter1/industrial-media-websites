8b8ce5c8745af7327bb621b677a6528231e27fd9
Jacob Bare <jacob@limit0.io>
2020-04-07 10:09:49 -0500

    Add LinkedIn, YouTube and Instagram social links

--- sites/cannabisequipmentnews.com/config/social-links.js
+++ sites/cannabisequipmentnews.com/config/social-links.js
@@ -1,4 +1,7 @@
 module.exports = [
   { provider: 'facebook', href: 'https://www.facebook.com/CannaEquipmentNews/' },
   { provider: 'twitter', href: 'https://twitter.com/news_cen' },
+  { provider: 'linkedin', href: 'https://www.linkedin.com/company/cannabis-equipment-news/' },
+  { provider: 'youtube', href: 'https://www.youtube.com/channel/UCe37Mhsr0aaBUuBzk1RR6AA/featured' },
+  { provider: 'instagram', href: 'https://www.instagram.com/cannabisequipmentnews/?hl=en' },
 ];
