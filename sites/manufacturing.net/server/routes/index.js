a4978f6f2bb0c147653d88446f30dbe8ad17ac2e
Jacob Bare <jacob@limit0.io>
2020-04-17 10:37:27 -0500

    Add videos route to ABM sites

--- sites/manufacturing.net/server/routes/index.js
+++ sites/manufacturing.net/server/routes/index.js
@@ -1,6 +1,7 @@
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
+const publishedContent = require('./published-content');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
@@ -13,6 +14,9 @@ module.exports = (app) => {
 
   // Content Pages
   content(app);
+
+  // Published Content
+  publishedContent(app);
 
   // Search
   search(app);
