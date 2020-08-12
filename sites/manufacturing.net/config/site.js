320bf36e1d006de278649977facc368c37af2e6f
Josh Worden <josh@parameter1.com>
2020-08-12 13:22:44 -0500

    Re-enable IdentityX

--- sites/manufacturing.net/config/site.js
+++ sites/manufacturing.net/config/site.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   identityX: {
     appId: '5e28a3dd58e67b229e55ae43',
-    enabled: false,
+    enabled: true,
     comments: { enabled: false },
     options: {
       appContextId: '5e83669a6eabae9f989e63c4',
