453324d102d66f5ef408ffcb8bc4232896602ea9
Brandon Krigbaum <brandonbk@gmail.com>
2021-02-27 13:59:08 -0600

    Add mnet topic placements

--- sites/manufacturing.net/config/native-x.js
+++ sites/manufacturing.net/config/native-x.js
@@ -1,5 +1,13 @@
 module.exports = {
   placements: {
     default: '6032ab022a687f00014eea53',
+    aerospace: '603aa3dd0057fb0001ef6f4a',
+    automotive: '603aa3f10057fb0001ef6fe8',
+    energy: '603aa4094a44ce0001690ff0',
+    industry40: '603aa4224a44ce000169108b',
+    labor: '603aa4384a44ce0001691124',
+    operations: '603aa44c0057fb0001ef7228',
+    software: '603aa4620057fb0001ef72af',
+    'supply-chain': '603aa4760057fb0001ef732e',
   },
 };
