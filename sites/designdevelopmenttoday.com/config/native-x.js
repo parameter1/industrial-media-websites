7e1e6a40d948d59c59c7c2335c1b835b3f0357df
Brandon Krigbaum <brandonbk@gmail.com>
2021-02-27 13:46:06 -0600

    Add ddt topic placements

--- sites/designdevelopmenttoday.com/config/native-x.js
+++ sites/designdevelopmenttoday.com/config/native-x.js
@@ -1,5 +1,14 @@
 module.exports = {
   placements: {
     default: '6032aa80e7c97d0001078716',
+    exclusives: '6039afb2e7c97d00013d949a',
+    'industries/aerospace': '6039afe4e7c97d00013d963b',
+    'industries/automotive': '6039b0042a687f000184f854',
+    'industries/manufacturing': '6039b024e7c97d00013d98e9',
+    'industries/medical': '6039b042e7c97d00013d9a64',
+    'industries/military': '6039b066e7c97d00013d9c31',
+    'new-products': '6039b081e7c97d00013d9d7f',
+    news: '6039b095e7c97d00013d9e71',
+    video: '6039b0b2e7c97d00013d9fd6',
   },
 };
