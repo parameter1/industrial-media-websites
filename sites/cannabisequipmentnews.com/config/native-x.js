3d9c0c591cef7d297391ca1f472d937679fee699
Brandon Krigbaum <brandonbk@gmail.com>
2021-03-10 09:12:34 -0600

    Disable native-x topics

--- sites/cannabisequipmentnews.com/config/native-x.js
+++ sites/cannabisequipmentnews.com/config/native-x.js
@@ -1,13 +1,13 @@
 module.exports = {
   placements: {
     default: '6032aa6ce7c97d00010786fa',
-    cultivation: '6039ad3fe7c97d00013d7c92',
-    distribution: '6039ad66e7c97d00013d7e16',
-    extraction: '6039add72a687f000184e537',
-    manufacturing: '6039ad242a687f000184dc7f',
-    'new-products': '6039acba2a687f000184d875',
-    news: '6039ac7ae7c97d00013d7552',
-    packaging: '6039acf52a687f000184dac5',
-    processing: '6039ad0c2a687f000184db90',
+    // cultivation: '6039ad3fe7c97d00013d7c92',
+    // distribution: '6039ad66e7c97d00013d7e16',
+    // extraction: '6039add72a687f000184e537',
+    // manufacturing: '6039ad242a687f000184dc7f',
+    // 'new-products': '6039acba2a687f000184d875',
+    // news: '6039ac7ae7c97d00013d7552',
+    // packaging: '6039acf52a687f000184dac5',
+    // processing: '6039ad0c2a687f000184db90',
   },
 };
--- sites/ien.com/config/native-x.js
+++ sites/ien.com/config/native-x.js
@@ -1,14 +1,14 @@
 module.exports = {
   placements: {
     default: '6032a882e7c97d000107867f',
-    automation: '603aa0080057fb0001ef503a',
-    'finishing-world': '603aa0210057fb0001ef514d',
-    'made-in-america': '603aa03c0057fb0001ef5232',
-    'new-products': '603aa0874a44ce000168f3bb',
-    operations: '603aa0a10057fb0001ef54d2',
-    'product-development': '603aa0be4a44ce000168f61e',
-    safety: '603aa0e24a44ce000168f73a',
-    software: '603aa1064a44ce000168f884',
-    'supply-chain': '603aa11f0057fb0001ef5981',
+    // automation: '603aa0080057fb0001ef503a',
+    // 'finishing-world': '603aa0210057fb0001ef514d',
+    // 'made-in-america': '603aa03c0057fb0001ef5232',
+    // 'new-products': '603aa0874a44ce000168f3bb',
+    // operations: '603aa0a10057fb0001ef54d2',
+    // 'product-development': '603aa0be4a44ce000168f61e',
+    // safety: '603aa0e24a44ce000168f73a',
+    // software: '603aa1064a44ce000168f884',
+    // 'supply-chain': '603aa11f0057fb0001ef5981',
   },
 };
