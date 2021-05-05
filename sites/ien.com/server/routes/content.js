e2b7656ef15154ea802e8aba720dac0225bf16d9
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-05 15:04:43 -0500

    Update content route regex

--- sites/ien.com/server/routes/content.js
+++ sites/ien.com/server/routes/content.js
@@ -3,7 +3,7 @@ const content = require('@industrial-media/package-shared/templates/content');
 const queryFragment = require('@industrial-media/package-shared/graphql/fragments/content-page');
 
 module.exports = (app) => {
-  app.get('/*?:id(\\d{8})*', withContent({
+  app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', withContent({
     template: content,
     queryFragment,
   }));
