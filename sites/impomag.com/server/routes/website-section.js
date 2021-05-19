f4558dadcf3f4a3565e43d378dc541be538bc053
Jessica Wade <jessica.wade@parameter1.com>
2021-05-19 14:06:21 -0500

    Update website video section

--- sites/impomag.com/server/routes/website-section.js
+++ sites/impomag.com/server/routes/website-section.js
@@ -1,11 +1,17 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const section = require('@industrial-media/package-shared/templates/website-section');
+const video = require('@industrial-media/package-shared/templates/website-section/video');
 const contactUs = require('@industrial-media/package-shared/templates/website-section/contact-us');
 const queryFragment = require('@industrial-media/package-shared/graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
+    queryFragment,
+  }));
+
+  app.get('/:alias(video)', withWebsiteSection({
+    template: video,
     queryFragment,
   }));
 
