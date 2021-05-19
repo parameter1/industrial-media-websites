80aa3706815dd9821ce70458e9baa29347cf8dd9
Jessica Wade <jessica.wade@parameter1.com>
2021-05-19 13:10:06 -0500

    Remove publish content route override-impo

--- sites/impomag.com/server/routes/index.js
+++ sites/impomag.com/server/routes/index.js
@@ -1,7 +1,6 @@
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
-const publishedContent = require('./published-content');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
@@ -14,9 +13,6 @@ module.exports = (app) => {
 
   // Content Pages
   content(app);
-
-  // Published Content
-  publishedContent(app);
 
   // Search
   search(app);
