f2dd58548dd2b3b10f849d83bf6469a418a21190
Brian Miller <brian@parameter1.com>
2021-06-01 12:36:46 -0500

    Add sitemap routes & pagination

--- packages/shared/start-server.js
+++ packages/shared/start-server.js
@@ -3,6 +3,8 @@ const { startServer } = require('@parameter1/base-cms-marko-web');
 const { set, get, getAsObject } = require('@parameter1/base-cms-object-path');
 const cleanResponse = require('@parameter1/base-cms-marko-core/middleware/clean-marko-response');
 const contactUsHandler = require('@industrial-media/package-contact-us');
+const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+const htmlSitemapRoutes = require('@parameter1/base-cms-marko-web-html-sitemap/routes');
 
 const buildNativeXConfig = require('./native-x/build-config');
 
@@ -19,6 +21,8 @@ const routes = siteRoutes => (app) => {
   userRoutes(app);
   // Load newsletter signup.
   newsletterRoute(app);
+  // HTML Sitemap
+  htmlSitemapRoutes(app);
   // Load site routes.
   siteRoutes(app);
 };
@@ -42,6 +46,9 @@ module.exports = (options = {}) => {
 
       // Setup NativeX.
       set(app.locals, 'nativeX', buildNativeXConfig(nativeXConfig));
+
+      // Use paginated middleware
+      app.use(htmlSitemapPagination());
 
       // Clean all response bodies.
       app.use(cleanResponse());
