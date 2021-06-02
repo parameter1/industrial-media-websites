a0d58274fedde611c96288b9e86c19bb3500bf17
Brian Miller <brian@parameter1.com>
2021-06-02 11:35:59 -0500

    split and register OmedaRapidIdentityX component per site

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -8,7 +8,6 @@ import PhotoSwipe from '@parameter1/base-cms-marko-web-photoswipe/browser';
 import ContactUs from '@industrial-media/package-contact-us/browser';
 import IdentityX from '@parameter1/base-cms-marko-web-identity-x/browser';
 import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
-import OmedaRapidIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser/rapid-identify.vue';
 
 const NewsletterSignup = () => import(/* webpackChunkName: "shared-newsletter-signup" */ './newsletter-signup/index.vue');
 
@@ -25,13 +24,4 @@ export default (Browser) => {
   P1Events(Browser);
 
   Browser.register('SharedNewsletterSignup', NewsletterSignup);
-  Browser.register('OmedaRapidIdentityX', OmedaRapidIdentityX, {
-    on: {
-      'encrypted-id-found': (encryptedId) => {
-        if (encryptedId && window.p1events) {
-          window.p1events('setIdentity', `omeda.indm.customer*${encryptedId}~encrypted`);
-        }
-      },
-    },
-  });
 };
