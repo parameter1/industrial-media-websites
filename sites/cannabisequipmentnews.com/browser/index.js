898d4d1b7762bd136a3b8e3b6c6032419a4ce32c
Jacob Bare <jacob@parameter1.com>
2021-11-12 10:06:05 -0600

    Fully register and rework Omeda+IDX browser components

--- sites/cannabisequipmentnews.com/browser/index.js
+++ sites/cannabisequipmentnews.com/browser/index.js
@@ -1,8 +1,6 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 import Shared from '@industrial-media/package-shared/browser';
-import Omeda from '@industrial-media/package-shared/browser/omeda-lynchm';
 
 Shared(Browser);
-Omeda(Browser);
 
 export default Browser;
