1f75e2d4649cd9c34d268d775c3c9f67c15092aa
Jessica Wade <jessica.wade@parameter1.com>
2022-03-08 13:36:29 -0600

    Add IdentityX ‘appContextId’

--- sites/medicaldesigndevelopment.com/config/identity-x.js
+++ sites/medicaldesigndevelopment.com/config/identity-x.js
@@ -3,6 +3,6 @@ const configureIdentityX = require('@industrial-media/package-shared/config/iden
 module.exports = ({ omedaBrandKey }) => configureIdentityX({
   appId: '5e28a3dd58e67b229e55ae43',
   comments: { enabled: false },
-  appContextId: '',
+  appContextId: '6227857913ad4d2769c59aea',
   omedaBrandKey,
 });
