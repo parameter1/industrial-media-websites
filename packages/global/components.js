1c9fab4f9e96529441fa615a54607b72d8042e88
Brian Miller <brian@parameter1.com>
2022-05-05 13:16:40 -0500

    initial addition of load-more

--- packages/global/components.js
+++ packages/global/components.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const GlobalContentListFeedFlow = require('./components/flows/content/list-feed');
+const GlobalContentCardDeckFeedFlow = require('./components/flows/content/card-deck-feed');
+// const GlobalMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
+
+module.exports = {
+  GlobalContentListFeedFlow,
+  GlobalContentCardDeckFeedFlow,
+  // GlobalMagazineIssueArchiveFlow,
+};
--- packages/global/fragments.js
+++ packages/global/fragments.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const globalContentList = require('./graphql/fragments/content-list');
+const globalContentLatest = require('./graphql/fragments/content-latest');
+// const globalMagazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
+
+module.exports = {
+  globalContentList,
+  globalContentLatest,
+  // globalMagazineIssueArchive,
+};
