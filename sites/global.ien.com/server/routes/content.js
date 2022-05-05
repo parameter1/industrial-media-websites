931645dca05a6035df22ec2fda858126ac545790
Brian Miller <brian@parameter1.com>
2022-05-05 10:30:28 -0500

    Add default empty right rail for now.

--- sites/global.ien.com/server/routes/content.js
+++ sites/global.ien.com/server/routes/content.js
@@ -2,8 +2,6 @@ const withContent = require('@industrial-media/package-global/middleware/with-co
 const queryFragment = require('@industrial-media/package-global/graphql/fragments/content-page');
 const contact = require('@industrial-media/package-global/templates/content/contact');
 const company = require('@industrial-media/package-global/templates/content/company');
-const product = require('@industrial-media/package-global/templates/content/product');
-const whitepaper = require('@industrial-media/package-global/templates/content/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
@@ -14,16 +12,6 @@ module.exports = (app) => {
 
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment,
-  }));
-
-  app.get('/*?product/:id(\\d{8})*', withContent({
-    template: product,
-    queryFragment,
-  }));
-
-  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
-    template: whitepaper,
     queryFragment,
   }));
 
