1cd27eaf8f7268eb82a4cea37938b5077e829759
Brian Miller <brian@parameter1.com>
2022-05-05 10:06:51 -0500

    add reveal ad components and package inclusion

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,8 +2,11 @@ import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
+const RevealAdHandler = () => import(/* webpackChunkName: "reveal-ad-handler" */ './reveal-ad-handler.vue');
+
 export default (Browser) => {
   GCSE(Browser);
   ContactUs(Browser);
   MonoRail(Browser);
+  Browser.register('RevealAdHandler', RevealAdHandler);
 };
