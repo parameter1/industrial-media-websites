c84d572813d2913507fad93bad5fd566ff30bc5e
Brian Miller <brian@parameter1.com>
2022-05-05 10:35:18 -0500

    update nativeX config

--- packages/global/config/native-x.js
+++ packages/global/config/native-x.js
@@ -1,6 +1,6 @@
 const NativeXConfiguration = require('@parameter1/base-cms-marko-web-native-x/config');
 
 module.exports = ({
-  uri = 'https://diverse.native-x.parameter1.com',
+  uri = 'https://indm.native-x.parameter1.com',
   enabled = false,
 } = {}) => new NativeXConfiguration(uri, { enabled });
