533e747cbd7fe9699ee38c730a8885deba82526e
Brian Miller <brian@parameter1.com>
2022-05-09 11:20:29 -0500

    add brightcove video component support
    Add vue/marko components

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,6 +1,7 @@
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
+import GlobalFloatingVideoPlayer from './floating-video-player.vue';
 
 const RevealAdHandler = () => import(/* webpackChunkName: "reveal-ad-handler" */ './reveal-ad-handler.vue');
 
@@ -8,5 +9,6 @@ export default (Browser) => {
   GCSE(Browser);
   ContactUs(Browser);
   MonoRail(Browser);
+  Browser.register('GlobalFloatingVideoPlayer', GlobalFloatingVideoPlayer);
   Browser.register('RevealAdHandler', RevealAdHandler);
 };
