f12c41f22da3e2dadad850643cff94d54b25425a
Brian Miller <brian@parameter1.com>
2022-05-09 11:59:28 -0500

    Consolidate content, home & section routes & templates
    move shared templates from site to global theme and update site routes to use shared templaes for home, content & default website-section calls

--- sites/global.ien.com/server/routes/content.js
+++ sites/global.ien.com/server/routes/content.js
@@ -1,10 +1,17 @@
 const withContent = require('@industrial-media/package-global/middleware/with-content');
 const queryFragment = require('@industrial-media/package-global/graphql/fragments/content-page');
+const company = require('@industrial-media/package-global/templates/content/company');
+const content = require('@industrial-media/package-global/templates/content');
 const { loaderQueryFragment, formatResponse } = require('@industrial-media/package-global/leads-content-handler');
-const content = require('../templates/content');
-
 
 module.exports = (app) => {
+  app.get('/*?company/:id(\\d{8})*', withContent({
+    template: company,
+    queryFragment,
+    loaderQueryFragment,
+    formatResponse,
+  }));
+
   app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', withContent({
     template: content,
     queryFragment,
