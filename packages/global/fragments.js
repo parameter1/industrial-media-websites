a7dfed6d09c50cbb4bcb14892caef286f3e1bad8
Brian Miller <brian@parameter1.com>
2022-05-12 14:06:42 -0500

    Update issue/publication/magazine landing pages
     - udpate to use dynamic loadmor and register components & fragments for that to work.
     - remove standard pagination used in other monorail setups

--- packages/global/fragments.js
+++ packages/global/fragments.js
@@ -1,9 +1,9 @@
 const globalContentList = require('./graphql/fragments/content-list');
 const globalContentLatest = require('./graphql/fragments/content-latest');
-// const globalMagazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
+const globalMagazineIssueArchive = require('./components/magazine/graphql/fragments/magazine-issue-archive');
 
 module.exports = {
   globalContentList,
   globalContentLatest,
-  // globalMagazineIssueArchive,
+  globalMagazineIssueArchive,
 };
