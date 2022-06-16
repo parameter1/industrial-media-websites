9bdca268eb230149e43ee0c8f640c56f4939d482
Brian Miller <brian@parameter1.com>
2022-06-16 09:55:04 -0500

    Initial setup of fm-global

---
+++ sites/global.foodmanufacturing.com/config/identity-x.js
@@ -0,0 +1,8 @@
+const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
+
+module.exports = ({ omedaBrandKey }) => configureIdentityX({
+  appId: '5e28a3dd58e67b229e55ae43',
+  comments: { enabled: false },
+  appContextId: '5e82041275a0950125baf2b6',
+  omedaBrandKey,
+});
---
+++ sites/global.foodmanufacturing.com/config/newsletter.js
@@ -0,0 +1,59 @@
+const privacyPolicy = require('./privacy-policy');
+
+const baseConfig = {
+  action: 'https://athleticbusiness.dragonforms.com/loading.do',
+  hiddenInputs: [
+    { name: 'omedasite', value: 'wfb_pref' },
+  ],
+};
+
+const defaults = {
+  name: 'NOT_SET',
+  description: 'NOT_SET',
+  privacyPolicy,
+  defaultNewsletter: {
+    deploymentTypeId: 0,
+    name: 'NOT_SET',
+    eventCategory: 'NOT_SET',
+  },
+  newsletters: [
+    {
+      deploymentTypeId: 0,
+      name: 'NOT_SET',
+      description: 'NOT_SET',
+      eventCategory: 'NOT_SET',
+    },
+  ],
+  demographic: {
+    id: 0,
+    label: 'Your primary role?',
+    values: [
+      { id: 0, label: 'NOT_SET' },
+    ],
+  },
+};
+
+module.exports = {
+  // uses inline omeda form
+  signupBanner: {
+    ...defaults,
+    imagePath: 'static/newsletter-pushdown/wfb-full.png',
+  },
+  pushdown: {
+    ...defaults,
+    imagePath: 'static/newsletter-pushdown/wfb-half.png',
+    description: 'NOT_SET',
+  },
+
+  // links off to seperate omeda dragonform
+  signupBannerLarge: {
+    ...baseConfig,
+    name: 'Newsletter for Wood Floor Professionals',
+    description: 'Get industry news, insights, and new product information delivered straight to your inbox.',
+  },
+  signupFooter: {
+    ...baseConfig,
+    name: 'Newsletter for Wood Floor Professionals',
+    description: 'Get industry news, insights, and new product information delivered straight to your inbox.',
+  },
+};
