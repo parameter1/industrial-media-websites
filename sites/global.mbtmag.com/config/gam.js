7f30be9f5634579cd2afdab585ba39b6ba565398
Jessica Wade <jessica.wade@parameter1.com>
2022-07-29 10:10:32 -0500

    Update ad units for new theme

--- sites/global.mbtmag.com/config/gam.js
+++ sites/global.mbtmag.com/config/gam.js
@@ -4,14 +4,14 @@ const config = configureGAM({ basePath: 'MBT' });
 
 config
   .setAliasAdUnits('default', [
-    { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'default_lb1' },
+    { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'default_billboard' },
     { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'default_lb1' },
     { name: 'rotation', templateName: 'LEADERBOARD', path: 'default_lb1' },
     // 300x250 that loads in the tops stories block on section & homepages
     { name: 'top-stories', templateName: 'RAIL', path: 'default_rail1' },
     { name: 'load-more', templateName: 'RAIL', path: 'default_rail1' },
-    { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'default_rail1' },
-    { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'default_rail1' },
+    { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
+    { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
     { name: 'reskin', path: 'default_reskin' },
   ]);
 
