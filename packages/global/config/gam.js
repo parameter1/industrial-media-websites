164a9caef92281786ac125ab2db51c0d5395a472
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-01 09:14:02 -0500

    Update GAM configs

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -6,53 +6,48 @@ module.exports = ({
 } = {}) => {
   const config = new GAMConfiguration(accountId, { basePath });
 
+  config.lazyLoad = {
+    enabled: true, // set to true to enable lazy loading
+    fetchMarginPercent: 100, // fetch ad when one viewport away
+    renderMarginPercent: 50, // render ad when half viewport away
+    mobileScaling: 2, // double these on mobile
+  };
+
   config
-    .setTemplate('LEADERBOARD', {
-      size: [
-        [970, 250],
-        [1060, 596],
-        [736, 414],
-        [428, 241],
-        [265, 149],
-      ],
-      sizeMapping: [
-        { viewport: [1070, 0], size: [[970, 250], [1060, 596]] },
-        { viewport: [980, 0], size: [[970, 250], [736, 414]] },
-        { viewport: [750, 0], size: [[300, 250], [736, 414]] },
-        { viewport: [300, 0], size: [[300, 250], [428, 241], [265, 149]] },
-      ],
-    })
     .setTemplate('LB-STICKY-BOTTOM', {
       size: [
         [970, 90],
-        [970, 66],
         [728, 90],
         [320, 50],
         [300, 50],
-        [300, 100],
       ],
-      sizeMapping: [
-        { viewport: [980, 0], size: [[970, 90], [970, 66], [728, 90]] },
-        { viewport: [750, 0], size: [728, 90] },
-        { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
-      ],
-    })
-    .setTemplate('RAIL', {
-      size: [300, 250],
-    })
-    .setTemplate('ROTATION', {
-      size: [[970, 90], [728, 90]],
       sizeMapping: [
         { viewport: [980, 0], size: [[970, 90], [728, 90]] },
-        { viewport: [750, 0], size: [[728, 90], [300, 250]] },
-        { viewport: [300, 0], size: [[300, 50], [300, 100], [300, 250]] },
+        { viewport: [750, 0], size: [728, 90] },
+        { viewport: [320, 0], size: [[300, 50], [320, 50]] },
+      ],
+    })
+    .setTemplate('BILLBOARD', {
+      size: [
+        [970, 250],
+        [300, 250],
+      ],
+      sizeMapping: [
+        { viewport: [980, 0], size: [970, 250] },
+        { viewport: [320, 0], size: [300, 250] },
+      ],
+    })
+    .setTemplate('ROTATION', {
+      size: [300, 250],
+      sizeMapping: [
+        { viewport: [320, 0], size: [300, 250] },
       ],
     })
     .setTemplate('INLINE-CONTENT-MOBILE', {
       size: [300, 250],
       sizeMapping: [
         { viewport: [980, 0], size: [] },
-        { viewport: [300, 0], size: [300, 250] },
+        { viewport: [320, 0], size: [300, 250] },
       ],
     })
     .setTemplate('INLINE-CONTENT-DESKTOP', {
--- sites/global.designdevelopmenttoday.com/config/gam.js
+++ sites/global.designdevelopmenttoday.com/config/gam.js
@@ -4,14 +4,12 @@ const config = configureGAM({ basePath: 'DDT' });
 
 config
   .setAliasAdUnits('default', [
-    { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'default_billboard' },
-    { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'default_lb1' },
-    { name: 'rotation', templateName: 'LEADERBOARD', path: 'default_lb1' },
-    // 300x250 that loads in the tops stories block on section & homepages
-    { name: 'top-stories', templateName: 'RAIL', path: 'default_rail1' },
-    { name: 'load-more', templateName: 'RAIL', path: 'default_rail1' },
-    { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
-    { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
+    { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'default_lb' },
+    { name: 'billboard', templateName: 'BILLBOARD', path: 'default_billboard' },
+    { name: 'rotation', templateName: 'ROTATION', path: 'default_rotation' },
+    { name: 'load-more', templateName: 'ROTATION', path: 'default_rotation' },
+    { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'default_rotation' },
+    { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'default_rotation' },
     { name: 'reskin', path: 'default_reskin' },
   ]);
 
