8710b4641ff66a1eff23aaf3552ce4ca3b30c5cd
Brian Miller <brian@parameter1.com>
2022-08-15 15:55:43 -0500

    Setup MBT site specific omeda/identityX configs

--- sites/mbtmag.com/config/identity-x.js
+++ sites/mbtmag.com/config/identity-x.js
@@ -1,8 +1,7 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
 
-module.exports = ({ omedaBrandKey }) => configureIdentityX({
+module.exports = configureIdentityX({
   appId: '5e28a3dd58e67b229e55ae43',
   comments: { enabled: false },
   appContextId: '5e8366b1da9f22f7ee6852b4',
-  omedaBrandKey,
 });
