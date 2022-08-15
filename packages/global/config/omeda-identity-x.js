a2a5b6c8f1ab23fe2d6479b87cc150ca2ceb5b41
Brian Miller <brian@parameter1.com>
2022-08-15 13:17:43 -0500

    update imcd behaviors

--- packages/global/config/omeda-identity-x.js
+++ packages/global/config/omeda-identity-x.js
@@ -4,6 +4,38 @@ const getBehaviors = ((key, websiteBehaviorAttributeId) => {
       behaviors: { logIn: 12, verifyEmail: 13, submitProfile: 14 },
       behaviorAttributes: {
         website: {
+          id: 3,
+          valueId: websiteBehaviorAttributeId,
+        },
+        actionSource: {
+          id: 2,
+          valueIds: {
+            default: 16105,
+            newsletterSignup: 16106,
+            comments: 16107,
+            contentGate: 16104,
+          },
+        },
+        newsletterSignupType: {
+          id: 4,
+          valueIds: {
+            default: 16111,
+            pushdown: 16109,
+            inlineContent: 16115,
+            inlineSection: 16113,
+            footer: 16108,
+          },
+        },
+        contentGateType: {
+          id: 5,
+          valueIds: { default: 16110, metered: 16112, printPreview: 16114 },
+        },
+      },
+    },
+    lynchm: {
+      behaviors: { logIn: 15, verifyEmail: 16, submitProfile: 17 },
+      behaviorAttributes: {
+        website: {
           id: 8,
           valueId: websiteBehaviorAttributeId,
         },
@@ -31,38 +63,6 @@ const getBehaviors = ((key, websiteBehaviorAttributeId) => {
           valueIds: { default: 16125, metered: 16126, printPreview: 16128 },
         },
       },
-    },
-    lynchm: {
-      behaviors: { logIn: 15, verifyEmail: 16, submitProfile: 17 },
-      behaviorAttributes: {
-        website: {
-          id: 8,
-          valueId: websiteBehaviorAttributeId,
-        },
-        actionSource: {
-          id: 7,
-          valueIds: {
-            default: 16130,
-            newsletterSignup: 16132,
-            comments: 16134,
-            contentGate: 16127,
-          },
-        },
-        newsletterSignupType: {
-          id: 6,
-          valueIds: {
-            default: 16129,
-            pushdown: 16124,
-            inlineContent: 16133,
-            inlineSection: 16131,
-            footer: 16123
-          }
-        },
-        contentGateType: {
-          id: 9,
-          valueIds: { default: 16125, metered: 16126, printPreview: 16128 },
-        },
-      }
     },
   };
   return behaviors[key];
