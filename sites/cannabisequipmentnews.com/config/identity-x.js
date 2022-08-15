a0033fcefff170313aa59f8f0e4a7875702ad844
Brian Miller <brian@parameter1.com>
2022-08-15 13:33:34 -0500

    Setup CEN site specific omeda/identityX configs

--- sites/cannabisequipmentnews.com/config/identity-x.js
+++ sites/cannabisequipmentnews.com/config/identity-x.js
@@ -1,8 +1,7 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
 
-module.exports = ({ omedaBrandKey }) => configureIdentityX({
+module.exports = configureIdentityX({
   appId: '5e28a3dd58e67b229e55ae43',
   comments: { enabled: true },
   appContextId: '5e8203df75a0950125baf2b5',
-  omedaBrandKey,
 });
