3e806ce1c6b8ad9960c6f679a9249c249ce4292b
Brian Miller <brian@parameter1.com>
2022-08-15 15:55:02 -0500

    Setup FM site specific omeda/identityX configs

--- sites/medicaldesigndevelopment.com/config/identity-x.js
+++ sites/medicaldesigndevelopment.com/config/identity-x.js
@@ -1,8 +1,7 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
 
-module.exports = ({ omedaBrandKey }) => configureIdentityX({
+module.exports = configureIdentityX({
   appId: '5e28a3dd58e67b229e55ae43',
   comments: { enabled: false },
   appContextId: '6227857913ad4d2769c59aea',
-  omedaBrandKey,
 });
