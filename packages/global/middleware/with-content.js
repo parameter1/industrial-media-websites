05dabe2c60b02f5225572c861a0b9d0ea4f8f113
Brian Miller <brian@parameter1.com>
2022-09-08 07:58:58 -0500

    merge leads-content with preset with-content middleware
    Instead of passing the format response & loaderQueryFragment into with content from lead-content-handler set the leads version as the default in with content.

--- packages/global/middleware/with-content.js
+++ packages/global/middleware/with-content.js
@@ -3,13 +3,29 @@ const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
 
 module.exports = (params = {}) => withContent({
   loaderQueryFragment: gql`
-    fragment WithContentPrimarySectionFragment on Content {
+    fragment WithContentTaxonomyCompanySectionFragment on Content {
       taxonomyIds
+      company {
+        id
+        name
+      }
       primarySection {
         id
         alias
       }
     }
   `,
+  formatResponse: ({ res, content: node }) => {
+    const tags = new Set();
+    tags.add('Website Content');
+    if (node.taxonomyIds.includes(3193399) || node.type === 'product') tags.add('PR');
+    if (node.type === 'video') tags.add('Video');
+    if (node.company && node.company.name) {
+      const companyName = encodeURIComponent(node.company.name);
+      res.set('x-lead-management-customer', companyName);
+    }
+    res.set('x-lead-management-link-type', 'Editorial');
+    res.set('x-lead-management-tags', [...tags].join(', '));
+  },
   ...params,
 });
