bc604cad843748f2ebd335690e33af8b13aa5d29
Jessica Wade <jessica.wade@parameter1.com>
2022-09-21 12:02:27 -0500

    Update ‘Advertise’ link to lynch media form-CEN

--- sites/cannabisequipmentnews.com/config/navigation.js
+++ sites/cannabisequipmentnews.com/config/navigation.js
@@ -15,7 +15,7 @@ const topics = sortNavItems([
 
 const secondary = [
   { href: '/video', label: 'Video' },
-  { href: 'https://ien.formstack.com/forms/advertise_with_industrial_media', label: 'Advertise', target: '_blank' },
+  { href: 'https://ien.formstack.com/forms/advertise_with_lynch_media', label: 'Advertise', target: '_blank' },
   { href: 'https://lynchmedia.dragonforms.com/loading.do?omedasite=cen_signup', label: 'Newsletter Signup', target: '_blank' },
   { href: '/podcast', label: 'Podcast' },
 ];
