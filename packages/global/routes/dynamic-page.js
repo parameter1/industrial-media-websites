d89bc2c5266eeb64e997b19a251bbd461abba9e7
Jessica Wade <jessica.wade@parameter1.com>
2023-02-17 08:55:26 -0600

    Update /contact-us route to use /page/contact-us

--- packages/global/routes/dynamic-page.js
+++ packages/global/routes/dynamic-page.js
@@ -1,11 +1,10 @@
 const { withDynamicPage } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/dynamic-page');
-const page = require('../templates/dynamic-page/index');
-const contactUs = require('../templates/dynamic-page/contact-us');
+const page = require('../templates/dynamic-page/contact-us');
 
 module.exports = (app) => {
-  app.get('/page/:alias(contact-us)', withDynamicPage({
-    template: contactUs,
+  app.get('/:alias(contact-us)', withDynamicPage({
+    template: page,
     queryFragment,
   }));
 
