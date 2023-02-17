7e7777fac409bb5e901d5d2e60baa1ece6cb8682
Jessica Wade <jessica.wade@parameter1.com>
2023-02-17 10:03:51 -0600

    Revert contact-us page routes

--- packages/global/routes/dynamic-page.js
+++ packages/global/routes/dynamic-page.js
@@ -1,10 +1,11 @@
 const { withDynamicPage } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/dynamic-page');
-const page = require('../templates/dynamic-page/contact-us');
+const page = require('../templates/dynamic-page/index');
+const contactUs = require('../templates/dynamic-page/contact-us');
 
 module.exports = (app) => {
-  app.get('/:alias(contact-us)', withDynamicPage({
-    template: page,
+  app.get('/page/:alias(contact-us)', withDynamicPage({
+    template: contactUs,
     queryFragment,
   }));
 
