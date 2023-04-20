9ff205cb6470c28c5305171bb916b7e9e7a0371c
Jacob Bare <jacob@parameter1.com>
2023-04-20 09:55:36 -0500

    Remove local reveal ad handler (it’s already loaded by monorail)

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -3,12 +3,9 @@ import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import GlobalFloatingVideoPlayer from './floating-video-player.vue';
 
-const RevealAdHandler = () => import(/* webpackChunkName: "reveal-ad-handler" */ './reveal-ad-handler.vue');
-
 export default (Browser) => {
   GCSE(Browser);
   ContactUs(Browser);
   MonoRail(Browser);
   Browser.register('GlobalFloatingVideoPlayer', GlobalFloatingVideoPlayer);
-  Browser.register('RevealAdHandler', RevealAdHandler);
 };
