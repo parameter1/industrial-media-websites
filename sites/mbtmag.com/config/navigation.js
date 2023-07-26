b339b92ddb2c985bce81a1b03d8552dfe0ba1748
Shinsina <shinsina@att.net>
2023-07-26 14:08:37 -0500

    Add Podcast to nav and menu, MBT

--- sites/mbtmag.com/config/navigation.js
+++ sites/mbtmag.com/config/navigation.js
@@ -15,10 +15,12 @@ const secondary = [
   { href: '/video', label: 'Video' },
   { href: 'https://ien.formstack.com/forms/advertise_with_industrial_media', label: 'Advertise', target: '_blank' },
   { href: 'https://industrialmedia.dragonforms.com/loading.do?omedasite=mbt_signup', label: 'Newsletter Signup', target: '_blank' },
+  { href: '/podcast', label: 'Podcast' },
 ];
 
 const resources = [
   { href: '/video', label: 'Video' },
+  { href: '/podcast', label: 'Podcast' },
   { href: '/page/mbt-about-us', label: 'About Us' },
   { href: '/page/contact-us', label: 'Contact Us' },
   { href: 'https://ien.formstack.com/forms/advertise_with_industrial_media', label: 'Advertise', target: '_blank' },
