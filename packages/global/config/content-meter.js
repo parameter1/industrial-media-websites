78665b70879cd46534f438b6a18cd983ab02020f
Brian Miller <brian@parameter1.com>
2023-12-04 16:07:27 -0600

    Add global content meter components, configs & css

---
+++ packages/global/config/content-meter.js
@@ -0,0 +1,21 @@
+module.exports = {
+  enabled: process.env.ENABLE_CONTENT_METER === 'true',
+  viewLimit: Number(process.env.CONTENT_METER_VIEW_LIMIT) || 1,
+  excludeLabels: [
+    'Sponsored',
+  ],
+  // excludeContentTypes: [
+  //   'company',
+  //   'contact',
+  // ],
+  // excludePrimarySectionIds: [
+  //   75347,
+  // ],
+  // excludePrimarySectionAlias: [
+  //   'home',
+  // ],
+  // 30 days to milliseconds
+  timeframe: 30 * 24 * 60 * 60 * 1000,
+  displayOverlay: true,
+  promoCode: 'registration_meter',
+};
--- packages/global/middleware/newsletter-state.js
+++ packages/global/middleware/newsletter-state.js
@@ -1,20 +1,75 @@
+const parser = require('ua-parser-js');
+const defaultValue = require('@parameter1/base-cms-marko-core/utils/default-value');
 const { get } = require('@parameter1/base-cms-object-path');
 
 const cookieName = 'enlPrompted';
 
-module.exports = () => (req, res, next) => {
-  const hasCookie = get(req, `cookies.${cookieName}`);
-  const utmMedium = get(req, 'query.utm_medium');
-  const olyEncId = get(req, 'query.oly_enc_id');
-  const fromEmail = utmMedium === 'email' || olyEncId || false;
+const newsletterState = ({ setCookie = true } = {}) => (req, res, next) => {
+  // account for site level enabling of initially expanded
+  const newsletterConfig = req.app.locals.site.getAsObject('newsletter');
+  if (newsletterConfig.pushdown) {
+    const { device } = parser(req.headers['user-agent']);
+    const disableMobileCBIE = defaultValue(newsletterConfig.pushdown.disableMobileCBIE, false);
+    const disableExpandOnMobile = disableMobileCBIE && (device && device.type === 'mobile');
+    const siteConfigCBIE = defaultValue(newsletterConfig.pushdown.canBeInitiallyExpanded, true);
+    const hasCookie = Boolean(get(req, `cookies.${cookieName}`));
+    const utmMedium = get(req, 'query.utm_medium');
+    const olyEncId = get(req, 'query.oly_enc_id');
+    const disabled = get(req, 'query.newsletterDisabled');
+    const fromEmail = utmMedium === 'email' || olyEncId || false;
+    const canBeInitiallyExpanded = siteConfigCBIE && !(
+      disableExpandOnMobile
+      || hasCookie
+      || fromEmail
+      || disabled
+    );
+    const initiallyExpanded = (setCookie === true) && canBeInitiallyExpanded;
 
-  if (!hasCookie) {
     // Expire in 14 days (2yr if already signed up)
     const days = fromEmail ? 365 * 2 : 14;
     const maxAge = days * 24 * 60 * 60 * 1000;
-    res.cookie(cookieName, true, { maxAge });
-  }
 
-  res.locals.newsletterState = { hasCookie, fromEmail };
+    if (initiallyExpanded) {
+      res.cookie(cookieName, true, { maxAge });
+    }
+
+    res.locals.newsletterState = {
+      hasCookie,
+      fromEmail,
+      disabled,
+      initiallyExpanded,
+      // set this for other middlewares to know it can be set later
+      // if formatContentResponse conditions are met
+      canBeInitiallyExpanded,
+      cookie: { name: cookieName, maxAge },
+    };
+  }
   next();
+};
+
+const formatContentResponse = ({ res, content }) => {
+  if (!res.locals.newsletterState) return;
+  const {
+    initiallyExpanded,
+    canBeInitiallyExpanded,
+    hasCookie,
+    fromEmail,
+    disabled,
+    cookie,
+  } = res.locals.newsletterState;
+
+  if (get(content, 'userRegistration.isCurrentlyRequired') === true) {
+    res.locals.newsletterState.initiallyExpanded = false;
+  } else if (
+    canBeInitiallyExpanded
+    && (!initiallyExpanded && !hasCookie && !disabled && !fromEmail)
+  ) {
+    res.cookie(cookie.name, true, { maxAge: cookie.maxAge });
+    res.locals.newsletterState.initiallyExpanded = true;
+  }
+};
+
+module.exports = {
+  newsletterState,
+  formatContentResponse,
 };
