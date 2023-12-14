b3638631b1a5d8eb4043c39bb2052d4288319186
Brian Miller <brian@parameter1.com>
2023-12-14 08:18:26 -0600

    Update lynch idx configs with custom questions

--- sites/cannabisequipmentnews.com/config/identity-x.js
+++ sites/cannabisequipmentnews.com/config/identity-x.js
@@ -1,4 +1,27 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
+const formDefault = require('@industrial-media/package-global/config/identity-x/default');
+
+// append custom demos row of questions to the end of general questions
+formDefault.fieldRows.push(
+  [
+    // Demo: Job Title
+    {
+      label: 'Job Title',
+      id: '657b03f46135407e614f1e8b',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+    // Demo: Primary Business
+    {
+      label: 'Primary Business',
+      id: '657b04b76135405c3a4f2a68',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+  ],
+);
 
 module.exports = configureIdentityX({
   comments: { enabled: false },
@@ -7,8 +30,20 @@ module.exports = configureIdentityX({
     'givenName',
     'familyName',
     'organization',
+    'organizationTitle', // @todo make this a custom field somehow
+    'street',
+    'city',
+    'regionCode', // Only require client-side for non-us/ca
     'countryCode',
+    'postalCode', // Only require client-side for non-us/ca
     '657b03f46135407e614f1e8b',
     '657b04b76135405c3a4f2a68',
   ],
+  activeCustomFieldIds: [
+    '657b03f46135407e614f1e8b',
+    '657b04b76135405c3a4f2a68',
+  ],
+  forms: {
+    default: formDefault,
+  },
 });
--- sites/medicaldesigndevelopment.com/config/identity-x.js
+++ sites/medicaldesigndevelopment.com/config/identity-x.js
@@ -1,6 +1,49 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
+const formDefault = require('@industrial-media/package-global/config/identity-x/default');
+
+// append custom demos row of questions to the end of general questions
+formDefault.fieldRows.push(
+  [
+    // Demo: Job Title
+    {
+      label: 'Job Title',
+      id: '657b03f46135407e614f1e8b',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+    // Demo: Primary Business
+    {
+      label: 'Primary Business',
+      id: '657b04b76135405c3a4f2a68',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+  ],
+);
 
 module.exports = configureIdentityX({
   comments: { enabled: false },
   appContextId: '6227857913ad4d2769c59aea',
+  requiredClientFields: [
+    'givenName',
+    'familyName',
+    'organization',
+    'organizationTitle', // @todo make this a custom field somehow
+    'street',
+    'city',
+    'regionCode', // Only require client-side for non-us/ca
+    'countryCode',
+    'postalCode', // Only require client-side for non-us/ca
+    '657b03f46135407e614f1e8b',
+    '657b04b76135405c3a4f2a68',
+  ],
+  activeCustomFieldIds: [
+    '657b03f46135407e614f1e8b',
+    '657b04b76135405c3a4f2a68',
+  ],
+  forms: {
+    default: formDefault,
+  },
 });
