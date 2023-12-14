14a8a837cdba9c2be6f29d203c1da60a895ec92c
Brian Miller <brian@parameter1.com>
2023-12-14 08:18:14 -0600

    Update indm idx configs with custom questions

--- packages/global/config/identity-x.js
+++ packages/global/config/identity-x.js
@@ -1,28 +1,5 @@
 const IdentityXConfiguration = require('@parameter1/base-cms-marko-web-identity-x/config');
 const newrelic = require('newrelic');
-const formDefault = require('./identity-x/default');
-
-// append custom demos row of questions to the end of general questions
-formDefault.fieldRows.push(
-  [
-    // Demo: Job Title
-    {
-      label: 'Job Title',
-      id: '60104e959fdc650033d91e91',
-      type: 'custom-select',
-      required: true,
-      width: 0.5,
-    },
-    // Demo: Primary Business
-    {
-      label: 'Primary Business',
-      id: '6575cf1827f0a7b1c503cd24',
-      type: 'custom-select',
-      required: true,
-      width: 0.5,
-    },
-  ],
-);
 
 module.exports = ({
   appId = '5e28a3dd58e67b229e55ae43',
@@ -47,12 +24,7 @@ module.exports = ({
     'familyName',
     'organization',
     'countryCode',
-    '642f689c5377824a6c1b851f',
-    '64301b173e81776bd5dc7cd8',
   ],
-  forms = {
-    default: formDefault,
-  },
   gtmUserFields = {
     primary_business: '60104e959fdc650033d91e91',
     job_title: '6575cf1827f0a7b1c503cd24',
@@ -66,7 +38,6 @@ module.exports = ({
     hiddenFields,
     requiredServerFields,
     requiredClientFields,
-    forms,
     gtmUserFields,
     onHookError: newrelic.noticeError.bind(newrelic),
     ...rest,
--- sites/inddist.com/config/identity-x.js
+++ sites/inddist.com/config/identity-x.js
@@ -1,6 +1,49 @@
 const configureIdentityX = require('@industrial-media/package-global/config/identity-x');
+const formDefault = require('@industrial-media/package-global/config/identity-x/default');
+
+// append custom demos row of questions to the end of general questions
+formDefault.fieldRows.push(
+  [
+    // Demo: Job Title
+    {
+      label: 'Job Title',
+      id: '60104e959fdc650033d91e91',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+    // Demo: Primary Business
+    {
+      label: 'Primary Business',
+      id: '6575cf1827f0a7b1c503cd24',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+  ],
+);
 
 module.exports = configureIdentityX({
   comments: { enabled: false },
   appContextId: '5e8366c2da9f229f3d6852b6',
+  requiredClientFields: [
+    'givenName',
+    'familyName',
+    'organization',
+    'organizationTitle', // @todo make this a custom field somehow
+    'street',
+    'city',
+    'regionCode', // Only require client-side for non-us/ca
+    'countryCode',
+    'postalCode', // Only require client-side for non-us/ca
+    '60104e959fdc650033d91e91',
+    '6575cf1827f0a7b1c503cd24',
+  ],
+  activeCustomFieldIds: [
+    '60104e959fdc650033d91e91',
+    '6575cf1827f0a7b1c503cd24',
+  ],
+  forms: {
+    default: formDefault,
+  },
 });
