aa080e2cbdce6fbf43a0363dd2397be56b2a349f
Shinsina <shinsina@att.net>
2024-01-03 08:04:23 -0600

    Include component in SSR registration

--- packages/global/browser/ssr.js
+++ packages/global/browser/ssr.js
@@ -1,5 +1,7 @@
 import ThemeComponents from '@parameter1/base-cms-marko-web-theme-monorail/browser/ssr';
+import brightcoveGAMPlayer from './brightcove-gam-player.vue';
 
 export default {
   ...ThemeComponents,
+  brightcoveGAMPlayer,
 };
