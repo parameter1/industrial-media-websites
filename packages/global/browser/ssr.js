80a17b02193aa8c1ea437da19897849a3bc5e39a
Shinsina <shinsina@att.net>
2024-01-03 16:07:12 -0600

    Remove Brightcove from SSR inclusion

--- packages/global/browser/ssr.js
+++ packages/global/browser/ssr.js
@@ -1,7 +1,5 @@
 import ThemeComponents from '@parameter1/base-cms-marko-web-theme-monorail/browser/ssr';
-import brightcoveGAMPlayer from './brightcove-gam-player.vue';
 
 export default {
   ...ThemeComponents,
-  brightcoveGAMPlayer,
 };
