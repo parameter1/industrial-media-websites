bc1bb7811432e7b358cc2e6820df92318b1bc2a4
Shinsina <shinsina@att.net>
2024-01-03 08:04:09 -0600

    Include component in browser register

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,10 +2,12 @@ import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import GlobalFloatingVideoPlayer from './floating-video-player.vue';
+import brightcoveGAMPlayer from './brightcove-gam-player.vue';
 
 export default (Browser) => {
   GCSE(Browser);
   ContactUs(Browser);
   MonoRail(Browser);
   Browser.register('GlobalFloatingVideoPlayer', GlobalFloatingVideoPlayer);
+  Browser.register('BrightCoveGAMPlayer', brightcoveGAMPlayer);
 };
