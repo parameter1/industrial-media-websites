eb074913e4b775d8f58e98cea8c8ccbea173571a
Jessica Wade <jessica.wade@parameter1.com>
2024-01-05 14:12:04 -0600

    Add section based nativeX ad slots

--- sites/manufacturing.net/config/native-x.js
+++ sites/manufacturing.net/config/native-x.js
@@ -5,6 +5,31 @@ const config = configureNativeX();
 config
   .setAliasPlacements('default', [
     { name: 'default', id: '6032ab022a687f00014eea53' },
+    { name: 'related-content', id: '6032ab022a687f00014eea53' },
+  ])
+  .setAliasPlacements('supply-chain', [
+    { name: 'related-content', id: '603aa4760057fb0001ef732e' },
+  ])
+  .setAliasPlacements('software', [
+    { name: 'related-content', id: '603aa4620057fb0001ef72af' },
+  ])
+  .setAliasPlacements('operations', [
+    { name: 'related-content', id: '603aa44c0057fb0001ef7228' },
+  ])
+  .setAliasPlacements('industry40', [
+    { name: 'related-content', id: '603aa4224a44ce000169108b' },
+  ])
+  .setAliasPlacements('energy', [
+    { name: 'related-content', id: '603aa4094a44ce0001690ff0' },
+  ])
+  .setAliasPlacements('cybersecurity', [
+    { name: 'related-content', id: '603aa4384a44ce0001691124' },
+  ])
+  .setAliasPlacements('automotive', [
+    { name: 'related-content', id: '603aa3f10057fb0001ef6fe8' },
+  ])
+  .setAliasPlacements('aerospace', [
+    { name: 'related-content', id: '603aa3dd0057fb0001ef6f4a' },
   ]);
 
 module.exports = config;
