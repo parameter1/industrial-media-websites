a613bde391f90de14cf5febe2535e81e8b7eb75f
Jessica Wade <jessica.wade@parameter1.com>
2024-04-17 07:33:40 -0500

    Add target=‘_blank’ to all social links

--- sites/foodmanufacturing.com/config/social-links.js
+++ sites/foodmanufacturing.com/config/social-links.js
@@ -1,6 +1,6 @@
 module.exports = [
-  { provider: 'facebook', href: 'https://www.facebook.com/foodmanufacturingnews/' },
-  { provider: 'twitter', href: 'https://twitter.com/foodmfgnews' },
-  { provider: 'linkedin', href: 'https://www.linkedin.com/company/food-manufacturing/?viewAsMember=true' },
-  { provider: 'youtube', href: 'https://www.youtube.com/channel/UCyuE60CauysF5HBfdgDaAqQ' },
+  { provider: 'facebook', href: 'https://www.facebook.com/foodmanufacturingnews/', target: '_blank' },
+  { provider: 'twitter', href: 'https://twitter.com/foodmfgnews', target: '_blank' },
+  { provider: 'linkedin', href: 'https://www.linkedin.com/company/food-manufacturing/?viewAsMember=true', target: '_blank' },
+  { provider: 'youtube', href: 'https://www.youtube.com/channel/UCyuE60CauysF5HBfdgDaAqQ', target: '_blank' },
 ];
