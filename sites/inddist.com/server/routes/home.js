6fba6f283a487c847e56705a2863e9025d4c2e1c
Jessica Wade <jessica.wade@parameter1.com>
2024-08-05 12:54:23 -0500

    Add new home route

--- sites/inddist.com/server/routes/home.js
+++ sites/inddist.com/server/routes/home.js
@@ -1,6 +1,6 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
-const home = require('@industrial-media/package-global/templates/home');
+const home = require('../templates/home');
 
 module.exports = (app) => {
   app.get('/', withWebsiteSection({
