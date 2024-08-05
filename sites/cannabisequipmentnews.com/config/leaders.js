163621dd0a291541393f54f48b7f40a9d633920b
Jessica Wade <jessica.wade@parameter1.com>
2024-08-05 09:12:04 -0500

    Add leaders.js file to CEN

---
+++ sites/cannabisequipmentnews.com/config/leaders.js
@@ -0,0 +1,11 @@
+const enabled = (process.env.LEADERS_ENABLED === 'true' || process.env.LEADERS_ENABLED === true) || false;
+
+module.exports = {
+  enabled,
+  title: 'Cannabis Connect',
+  alias: process.env.LEADERS_ALIAS || 'leaders-2024',
+  header: {
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress',
+    imgSrcset: process.env.LEADERS_LOGO || 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress&dpr=2 2x',
+  },
+};
