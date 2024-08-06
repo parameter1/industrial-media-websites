d72419852de269414ec14cdd1c071d16ca900e6d
Brian Miller <brian@parameter1.com>
2024-08-06 07:31:02 -0500

    update leaders alias

--- sites/cannabisequipmentnews.com/config/leaders.js
+++ sites/cannabisequipmentnews.com/config/leaders.js
@@ -3,7 +3,7 @@ const enabled = (process.env.LEADERS_ENABLED === 'true' || process.env.LEADERS_E
 module.exports = {
   enabled,
   title: 'Cannabis Connect',
-  alias: process.env.LEADERS_ALIAS || 'leaders-2024',
+  alias: process.env.LEADERS_ALIAS || 'leaders/2025',
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress',
     imgSrcset: process.env.LEADERS_LOGO || 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress&dpr=2 2x',
