e5f138a2a641d9bd06febf7085249e3c7eacd16a
Shinsina <shinsina@att.net>
2024-08-20 10:55:16 -0500

    Fix import order

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,9 +1,9 @@
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
+import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 import GlobalFloatingVideoPlayer from './floating-video-player.vue';
 import BrightcoveGAMPlayer from './brightcove-gam-player.vue';
-import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 
 export default (Browser) => {
   GCSE(Browser);
