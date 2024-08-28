bf6c3bedd9cd2aaa66f8b8994e96b8d1a0467858
Jessica Wade <jessica.wade@parameter1.com>
2024-08-28 09:04:43 -0500

    Remove header for leaders

--- sites/cannabisequipmentnews.com/config/leaders.js
+++ sites/cannabisequipmentnews.com/config/leaders.js
@@ -4,9 +4,4 @@ module.exports = {
   enabled: true,
   title: 'Cannabis Connect',
   alias: process.env.LEADERS_ALIAS || 'leaders/2025',
-  header: {
-    imgSrc: 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress',
-    // imgSrcset:
-    // 'https://img.cannabisequipmentnews.com/files/base/indm/all/site-logos/cen.svg?h=70&auto=format,compress&dpr=2 2x',
-  },
 };
